8c47cedac457710e1f911006b8f3971dd2447417
ajay26188 <ajaysah52@gmail.com>
2024-01-24 19:32:14 +0200

    last commit

---
+++ src/components/MyContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MyContainer = () => {
+  return (
+    <div>
+        <h1>MyContainer</h1>
+        <p>This is a MyContainer page.</p>
+    </div>
+  )
+}
+
+export default MyContainer
