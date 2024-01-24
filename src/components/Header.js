b9b9ba0a42df48b0684529a7fb655c52698e2438
ajay26188 <ajaysah52@gmail.com>
2024-01-24 22:24:19 +0200

    last commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import  AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Button from '@mui/material/Button'
 
 const Header = () => {
   return (
     <div>
         <h1>Header</h1>
         <p>This is a Header page.</p>
+        <AppBar>
+            <Toolbar>
+                <Button element= {Link} to="/">
+                    Home
+                </Button>
+                <Button element= {Link} to="/about">
+                    About
+                </Button>
+            </Toolbar>
+        </AppBar>
     </div>
   )
 }
 
-export default Header
+export default Header;
