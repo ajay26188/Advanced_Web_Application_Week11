b7ab30cb9b2fbf5a9e487a00d0e90f5b64dca61d
ajay26188 <ajaysah52@gmail.com>
2024-02-02 14:47:25 +0200

    last commit

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
           <Route path ="/" element={<MyContainer />} />
           <Route path ="/about" element={<About />} />
         </Routes>
-        <WrappedMyContainer />
     </div>
     </Router>
   );
