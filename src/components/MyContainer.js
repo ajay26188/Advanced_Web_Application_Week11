e057d41bd5f3c34a6c8e118a71bb70bb57f9db9c
ajay26188 <ajaysah52@gmail.com>
2024-02-04 15:30:07 +0200

    last commit

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import MyHOC from './MyHOC';
 
 const MyContainer = ({name}) => {
   return (
     <div>
         <h1>MyContainer</h1>
         <p>This is a MyContainer page.</p>
-        <p>Hello, {name}!</p>
     </div>
   )
 }
