a15c950af513051974a59a69c6d9c992ff4c9135
ajay26188 <ajaysah52@gmail.com>
2024-02-04 20:37:09 +0200

    last commit

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import MyHOC from './MyHOC';
+import About from './About';
 
+const wrappedAbout = MyHOC(About, {name: "Kalle"});
 
 const MyContainer = () => {
   return (
     <div>
+      <wrappedAbout />
     </div>
   )
 }
